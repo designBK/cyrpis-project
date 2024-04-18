e49d87d709a6c57663f367e3abca22485b4cb9ab
designBK <benjaminkaplandesign@gmail.com>
2024-04-18 17:22:11 -0600

    setup sass files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import {CategoryScale, Chart, LinearScale, BarElement} from 'chart.js'; 
@@ -10,6 +10,7 @@ Chart.register(BarElement);
 
 function App() {
   const [papers, setPapers] = useState(null);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {fetchPapers("https://api.core.ac.uk/v3/search/works")}, []);
 
@@ -17,6 +18,15 @@ function App() {
     const response = await fetch(url);
     const results = await response.json();
     setPapers(results.results);
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const filterChanges = () => {
+    const url = 'https://api.core.ac.uk/v3/search/works?q=('+ inputValue + ')';
+    fetchPapers(url);
   }
 
   if(!papers) {
@@ -38,6 +48,10 @@ function App() {
     <h1>Papers</h1>
     <div className='chart-container'>
       <Bar className='chart' data={chartData}/>
+    </div>
+    <div className='filter-row'>
+      <input type="text" placeholder='Filters' onChange={handleInputChange}/>
+      <button onClick={filterChanges}>Filter</button>
     </div>
      <div className='card-container'>
         {papers.map((data) => {
